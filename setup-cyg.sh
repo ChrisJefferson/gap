fce1d45b3a466cef9b52ed47436cd765968d075c
Chris Jefferson <chris@bubblescope.net>
2018-05-23 21:06:16 +0100

    Updates

--- setup-cyg.sh
+++ setup-cyg.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-set -ex
 # Start doing a standard GAP build
 
 ./autogen.sh && ./configure && make -j4 && make bootstrap-pkg-full && cd pkg && ../bin/BuildPackages.sh
 
 # Grab some extra files
 
-for i in
-cygiconv-2.dll cyggmp-10.dll cygintl-8.dll cygreadline7.dll cygwin1.dll # Basics
- cygncursesw-10.dll cygncurses-10.dll cygpanel-10.dll cygpanelw-10.dll # Browse
- cygstdc++-6.dll # C++ (json / semigroups)
-  cyggcc*.dll # Exception handling (semigroups)
-; do
+BASICS="cygiconv-2.dll cyggmp-10.dll cygintl-8.dll cygreadline7.dll cygwin1.dll" # Basics
+BROWSE="cygncursesw-10.dll cygncurses-10.dll cygpanel-10.dll cygpanelw-10.dll" # Browse
+CPP="cygstdc++-6.dll" # C++ (json / semigroups)
+for i in ${BASICS} ${BROWSE} ${CPP} ${EXCEPTION}; do
  cp /usr/bin/$i .
+done;
+
+# Exception handling (semigroups)
+for i in /usr/bin/cyggcc*.dll; do
+  cp $i .
 done;
 
 for i in bash.exe  cygpath.exe   mintty.exe  sh.exe; do
