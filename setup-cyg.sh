273c38826e02a834ec6f2d83f70d10b29d1a5331
Chris Jefferson <chris@bubblescope.net>
2018-05-25 13:41:12 +0100

    Update setup-cyg.sh

--- setup-cyg.sh
+++ setup-cyg.sh
@@ -2,7 +2,7 @@
 
 # Start doing a standard GAP build
 
-./autogen.sh && ./configure && make -j4 && make bootstrap-pkg-full && cd pkg && ../bin/BuildPackages.sh
+./autogen.sh && ./configure && make -j4 && make bootstrap-pkg-full && cd pkg && ../bin/BuildPackages.sh && ../bin/CygwinPackageHacks.sh
 
 # Grab some extra files
 
