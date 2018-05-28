3902dc04c534d979249a57864187bbf7eeb94dfc
Chris Jefferson <chris@bubblescope.net>
2018-05-28 13:57:48 +0100

    Improve carat

--- bin/CygwinPackageHacks.sh
+++ bin/CygwinPackageHacks.sh
@@ -8,6 +8,12 @@
     find . -type f -name "*.c" -print0 | xargs -0 sed -i '' -e 's/itoa/carat_ITOA/g'
     find . -type f -name "*.h" -print0 | xargs -0 sed -i '' -e 's/itoa/carat_ITOA/g'
     make
+    # The name 'Con' upsets windows.
+
+    rm carat/bin/*/Con.exe carat/src/con.c
+    # Rename Con.html to ConCarat.html
+    find . -type f -name "*.html" -print0 | xargs -0 sed -i '' -e 's/Con.html/ConCarat.html/g'
+    mv carat/tex/progs/Con.html carat/tex/progs/ConCarat.html
 )
 
 # nauty22's configure doesn't understand cygwin
@@ -29,11 +35,4 @@
     ./configure &&
     make &&
     cp bin/lib/cyg* ../../.libs 
-)
-
-# The name 'Con' upsets windows. It seems this program isn't ever used
-(
-    # carat
-    cd carat
-    rm bin/*/Con.exe
 )
